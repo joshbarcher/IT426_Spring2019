9367dd58d0c6898ca6a35f8e0197f48940abce1e
Josh Archer <jarcher@greenriver.edu>
2019-04-09 19:14:48 -0700

    Finished our example on switching scenes with Java FX.

--- MyFirstJavaFXApp/src/switching_scenes/SceneSwitcher.java
+++ MyFirstJavaFXApp/src/switching_scenes/SceneSwitcher.java
@@ -3,16 +3,15 @@ package switching_scenes;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-import java.sql.Time;
 
 public class SceneSwitcher extends Application
 {
@@ -21,7 +20,7 @@ public class SceneSwitcher extends Application
     private Stage stage;
 
     @Override
-    public void start(Stage stage) throws Exception
+    public void start(Stage stage)
     {
         this.stage = stage;
 
@@ -45,19 +44,52 @@ public class SceneSwitcher extends Application
 
         ProgressIndicator progress = new ProgressIndicator();
         Text message = new Text("Please wait...");
+        message.setId("waiting-message");
+        message.getStyleClass().add("highlight");
 
         panel.getChildren().addAll(progress, message);
 
-        return new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
+        Scene scene = new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
+        scene.getStylesheets().add("styles.css");
+        return scene;
     }
 
     private Scene getBody()
     {
-        return new Scene(new VBox(), WIN_WIDTH, WIN_HEIGHT);
+        VBox panel = getMessagePanel();
+
+        Button button = new Button("Continue!");
+        panel.getChildren().add(button);
+
+        button.setOnAction(event -> {
+            stage.setScene(getEnding());
+        });
+
+        Scene scene = new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
+        scene.getStylesheets().add("styles.css");
+        return scene;
+    }
+
+    private VBox getMessagePanel()
+    {
+        VBox panel = new VBox();
+        panel.setSpacing(10);
+        panel.setAlignment(Pos.CENTER);
+        panel.setPadding(new Insets(10));
+
+        Text message = new Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Vivamus fermentum ut enim sit amet mattis. Donec pulvinar tortor at felis " +
+                "rhoncus, in aliquam nunc auctor. Pellentesque ut laoreet odio, nec " +
+                "molestie nunc. Sed eget est sodales, mattis metus ut, efficitur nunc.");
+        message.setWrappingWidth(WIN_WIDTH);
+
+        panel.getChildren().add(message);
+
+        return panel;
     }
 
     private Scene getEnding()
     {
-        return null;
+        return new Scene(getMessagePanel(), WIN_WIDTH, WIN_HEIGHT);
     }
 }
